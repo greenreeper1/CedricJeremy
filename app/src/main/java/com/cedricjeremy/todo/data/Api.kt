ddfd9dbe7b643dc847eef581deada5e0d6bdac8b
greenreeper1 <cedricba.gautier@gmail.com>
2024-12-27 16:18:26 +0100

    step 4 of TP3

---
+++ app/src/main/java/com/cedricjeremy/todo/data/Api.kt
@@ -0,0 +1,39 @@
+package com.cedricjeremy.todo.data
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+
+object Api {
+    private const val TOKEN = "76ef92afc5631fe8178ea9a3dd4e46133227ac6b"
+
+    private val retrofit by lazy {
+        // client HTTP
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .addInterceptor { chain ->
+                // intercepteur qui ajoute le `header` d'authentification avec votre token:
+                val newRequest = chain.request().newBuilder()
+                    .addHeader("Authorization", "Bearer $TOKEN")
+                    .build()
+                chain.proceed(newRequest)
+            }
+            .build()
+
+        // transforme le JSON en objets kotlin et inversement
+        val jsonSerializer = Json {
+            ignoreUnknownKeys = true
+            coerceInputValues = true
+        }
+
+        // instance retrofit pour implémenter les webServices:
+        Retrofit.Builder()
+            .baseUrl("https://api.todoist.com/")
+            .client(okHttpClient)
+            .addConverterFactory(jsonSerializer.asConverterFactory("application/json".toMediaType()))
+            .build()
+    }
+}
