51b626d2447fa492865848ce5a899989d4108f9c
greenreeper1 <cedricba.gautier@gmail.com>
2024-12-27 16:20:52 +0100

    step 5 of TP3

---
+++ app/src/main/java/com/cedricjeremy/todo/data/User.kt
@@ -0,0 +1,12 @@
+package com.cedricjeremy.todo.data
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class User( @SerialName("email")
+                 val email: String,
+                 @SerialName("full_name")
+                 val name: String,
+                 @SerialName("avatar_medium")
+                 val avatar: String? = null)
